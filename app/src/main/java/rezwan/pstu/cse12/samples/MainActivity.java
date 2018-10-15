38282d53298ad8006f757c93235fd33d0d5a33ad
rrsaikat <rrsaikat88@gmail.com>
2018-10-15 19:53:31 +0600

    //initial commit

---
+++ app/src/main/java/rezwan/pstu/cse12/samples/MainActivity.java
@@ -0,0 +1,52 @@
+package rezwan.pstu.cse12.samples;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.rezwan.pstu.cse12.view.CircularMorphLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    private CircularMorphLayout cmLayout;
+    private TextView mStartButtonTxt;
+    private ProgressBar pb;
+    private Button btStop;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        cmLayout = (CircularMorphLayout)findViewById(R.id.cml_proceed_layout);
+        pb =(ProgressBar)findViewById(R.id.progressBar);
+        btStop =(Button)findViewById(R.id.btnStop);
+        mStartButtonTxt = (TextView)findViewById(R.id.tv_proceed);
+        mStartButtonTxt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cmLayout.setDuration(800);
+                cmLayout.revealFrom(mStartButtonTxt.getWidth() / 2f,
+                        mStartButtonTxt.getHeight() / 2f,
+                        mStartButtonTxt.getWidth() / 2f,
+                        mStartButtonTxt.getHeight() / 2f).setListener(
+                        () -> {
+                            mStartButtonTxt.setVisibility(View.GONE);
+                            pb.setVisibility(View.VISIBLE);
+                        }).start();
+
+            }
+        });
+
+        btStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mStartButtonTxt.setVisibility(View.VISIBLE);
+                pb.setVisibility(View.INVISIBLE);
+                cmLayout.reverse();
+            }
+        });
+    }
+}
